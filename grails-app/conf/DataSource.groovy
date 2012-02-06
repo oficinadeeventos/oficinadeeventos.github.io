bdef0c91e51045a11da5d68db4e69340722dae7f
cleydson.fibonacci <cleydson.fibonacci@solucoesageis.com.br>
2012-02-06 02:40:57 +0000

    Modificando senha mysql
    git-svn-id: https://subversion.assembla.com/svn/manager-ofc/SiteOficinaDeEventos@52 a2ba9a23-d257-4511-9ec4-c5e08954d966

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
-    username = "cleydson"
-    password = "poraodorock"
+    username = "root"
+    password = "cj203019"
 }
 hibernate {
     cache.use_second_level_cache = true
